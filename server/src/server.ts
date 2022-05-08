4020f0bbe441f5f52250e4e96219bc9069123083
Raphael Duarte <raphafold@gmail.com>
2022-05-08 19:34:13 -0300

    configure port

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen( process.env.$PORT || 3333, ()=>{
+app.listen( process.env.$PORT || 5969, ()=>{
     console.log('HTTP server running!')
 })
